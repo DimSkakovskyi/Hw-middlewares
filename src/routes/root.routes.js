83efcc792246f14f2b62f2d3f5d54d6a6d624cbb
DimSkakovskyi <dimonskak@ukr.net>
2026-01-26 21:38:46 +0200

    Added configuration for Docker

--- src/routes/root.routes.js
+++ src/routes/root.routes.js
@@ -4,7 +4,7 @@ const { logRequests } = require("../middlewares/logRequests");
 router.use(logRequests);
 
 router.get("/", (req, res) => {
-  return res.type("text").send("Get root route");
+  return res.type("text").send("Get new root route"); в
 });
 
 module.exports = router;
